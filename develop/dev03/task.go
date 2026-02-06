e607a6f86acc2b15eb14dd39a86e4a634cc847af
al-faqun <noreply>
2026-02-07 01:08:11 +0300

    dev03: WIP2

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"slices"
+	"strings"
+	"golang.org/x/text/collate"
+	"golang.org/x/text/language"
+)
+
 /*
 === Утилита sort ===
 
@@ -25,17 +32,43 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+const SEP = "\n"
+
+/* Does not support windows separators for simplicity */
 func main() {
 
 }
 
 func sort(input string, options SortOptions) (string, error) {
+	// without options
+	lines := strings.Split(input, SEP)
 
+	col := createCol(options)
+
+	sortDec := func (a string, b string) int {
+		return defSort(a, b, col)
+	}
+
+	if !slices.IsSortedFunc(lines, sortDec) {
+		slices.SortStableFunc(lines, sortDec)
+	}
+
+	return strings.Join(lines, SEP), nil
+}
+
+func defSort(a string, b string, col *collate.Collator) int {
+	// strings.Compare doesn't work, as it does not respect language rules
+	return strings.Compare(a, b)
+}
+
+func createCol(options SortOptions) *collate.Collator {
+	// todo: for numeric collator see https://pkg.go.dev/golang.org/x/text/collate#example-New
+	return collate.New(language.English)
 }
 
 type SortOptions struct {
-	col int
-	isNum bool
-	isRev bool
+	col      int
+	isNum    bool
+	isRev    bool
 	isRemDub bool
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -7,32 +7,33 @@ import (
 func TestUnpackValid(t *testing.T) {
 	dataValid := []struct {
 		name     string
+		options  SortOptions
 		input    string
 		expected string
 	}{
 		{
-			name: "Default alpha sort",
-			options: SortOptions{},
-			input: "MX Linux\nManjaro\nMint\nelementary\nUbuntu",
-			expected: "elementary\nManjaro\nMint\nMX Linux\nUbuntu"
+			name:     "Default alpha sort",
+			options:  SortOptions{},
+			input:    "MX Linux\nManjaro\nMint\nelementary\nUbuntu",
+			expected: "elementary\nManjaro\nMint\nMX Linux\nUbuntu",
 		},
 		{
-			name: "Numeric sort",
-			options: SortOptions{},
-			input: "1\n10\n2\n21\n23\n3\n432\n5\n5\n60",
-			expected: "1\n2\n3\n5\n5\n10\n21\n23\n60\n432" // todo: check how linux sort -n reacts to text 
+			name:     "Numeric sort",
+			options:  SortOptions{isNum: true},
+			input:    "1\n10\n2\n21\n23\n3\n432\n5\n5\n60\nUbuntu\nelementary",
+			expected: "elementary\nUbuntu\n1\n2\n3\n5\n5\n10\n21\n23\n60\n432",
 		},
 		{
-			name: "Reverse sort",
-			options: SortOptions{},
-			input: "1.MX Linux\n4.elementary\n2.Manjaro\n5.Ubuntu\n3.Mint",
-			expected: "4.elementary\n1.MX Linux\n2.Manjaro\n5.Ubuntu\n3.Mint"
+			name:     "Reverse sort",
+			options:  SortOptions{isRev: true},
+			input:    "1.MX Linux\n4.elementary\n2.Manjaro\n5.Ubuntu\n3.Mint",
+			expected: "4.elementary\n1.MX Linux\n2.Manjaro\n5.Ubuntu\n3.Mint",
 		},
 		{
-			name: "Remove duplicates & sort",
-			options: SortOptions{},
-			input: "1.MX Linux\n2.Manjaro\n3.Mint\n4.elementary\n5.Ubuntu\n1.MX Linux\n2.Manjaro\n3.Mint\n4.elementary\n5.Ubuntu",
-			expected: "1.MX Linux\n2.Manjaro\n3.Mint\n4.elementary\n5.Ubuntu"
+			name:     "Remove duplicates & sort",
+			options:  SortOptions{isRemDub: true},
+			input:    "1.MX Linux\n2.Manjaro\n3.Mint\n4.elementary\n5.Ubuntu\n1.MX Linux\n2.Manjaro\n3.Mint\n4.elementary\n5.Ubuntu",
+			expected: "1.MX Linux\n2.Manjaro\n3.Mint\n4.elementary\n5.Ubuntu",
 		},
 		// {
 		// 	name: "Month sort",
@@ -44,7 +45,7 @@ func TestUnpackValid(t *testing.T) {
 
 	for _, data := range dataValid {
 		t.Run(data.name, func(t *testing.T) {
-			actual, err := unpack(data.input)
+			actual, err := sort(data.input, data.options)
 
 			if err != nil {
 				t.Fatalf("Unexpected error: '%q'\n", err.Error())
